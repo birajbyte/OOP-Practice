83b5d5e6e79404269f9f7961a9b2faa1bd0e0a69
Biraj <birajpathak83@gmail.com>
2026-06-05 09:32:15 +0545

    comfortable with basic like . self _init_ method

--- day_01/basic.py
+++ day_01/basic.py
@@ -1,17 +1,48 @@
 class Student:
-    # The setup instructions
-    def __init__(self,name,roll):
-        self.name = name  # Saves the name inside the specific object
-        self.roll = roll
-    def say_hello(self):
-        return f"Hello, my name is {self.name}"
-# Creating objects with unique names
-user_input1 = input("Enter the name of Student ")
-student1 = Student(user_input1,3)
-user_input2 = input("Enter the name of student ")
-student2 = Student(user_input2,4)
+    def __init__(self,name,faculty):
+        self.name = name
+        self.faculty = faculty
+    
+    def view(self):
+        return f"Student name {self.name} | Faculty {self.faculty}"
+    def __repr__(self):
+        return f"Student('{self.name}','{self.faculty}')"
+  #Store the data  
+classroom = []
+for i in range(3):
+    print(f"The details of student {i+1}")
+    user_input1 = input("Enter the name ")
+    user_input2 = input("Enter the Faculty ")
+    new_student = Student(user_input1,user_input2)
+    classroom.append(new_student)
+#now process to see the data
 
-# Viewing the output
-print(student1.say_hello())  # Output: Alice
-print(student2.say_hello()) 
-print(student1.roll)
+print(f"\n{"="*100}")
+print("                 CLASSROOM B-TECH                             ")
+print(f"\n{"="*100}")
+for student in classroom:
+    print(student.view())
+
+print(f"/n{"="*100}")
+print("                 SEARCH SYSTEM                                            ")
+print(f"/n{"="*100}")
+query = input("Enter the name of to  search in classroom  ")
+for student in classroom:
+    if student.name.lower() == query.lower():
+        print("\n Match found")
+        print(student.view())
+        break
+else:
+    print("Error not found enter correct name")
+print(f"/n{"="*100}")
+print("        Delete System                        ")
+query_delete = input("Enter the name to remove ")
+for student in classroom:
+    if student.name.lower() == query_delete.lower():
+        classroom.remove(student)
+        print(f"Student record {student.name} deleted")
+        break
+else:
+    print("Enter the valid name ")
+
+print(classroom)
