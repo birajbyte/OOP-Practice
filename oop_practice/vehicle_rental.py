3e8682f7f2165361b7ef5f0e80850dfd026232a4
Biraj <birajpathak83@gmail.com>
2026-06-17 21:41:00 +0545

    completed one project vehicle_rental.py concept unlocked more super()

--- oop_practice/vehicle_rental.py
+++ oop_practice/vehicle_rental.py
@@ -1,5 +1,5 @@
 #vehile management system
-class Vehicle:
+class Vehicle:#keep variable and parameter name same like self.is_avaiable = is_avaiable
 
     def __init__(self,brand,model,rate,is_avaiable = True):
         self.brand = brand
@@ -17,7 +17,7 @@ class Vehicle:
             self.__rental_rate = new_rate
             print(f"Rental rate for model {self.model} implemented: {new_rate}")
     def display_features(self):
-        print(f"Brand:{self.brand}|Model:{self.model}|Rate:{self.get_rental_rate()}")
+        print(f"Brand:{self.brand}|Model:{self.model}|Rate:{self.get_rental_rate()} per hour")
 
 class Car(Vehicle):
     def __init__(self, brand, model, rate, is_avaiable=True, doors = 4):
@@ -53,19 +53,45 @@ class RentalStore:
         else:
             for v in self.fleet:
                 v.display_features()
+    
+    def rent_vehicle(self , model , days):
+        if not self.fleet:
+            print("First add vehicle to store!!")
+            return
+        for vehicle in self.fleet:
+            if vehicle.model.lower()== model.lower():
+                if vehicle.avaiable:
+                    vehicle.avaiable = False
+                    total_cost = vehicle.get_rental_rate() * days * 24
+                    print(f"Total rent_cons {total_cost}  for {days} days ")
+                    return
+                else:
+                    print(f"The required vehicle with model:{vehicle.model} is already rented out ")
+                    return
+        print(f"Required vehicle model not found in {self.name}")
                 
+    def return_vehicle(self, model):
+        for vehicle in self.fleet:
+            if vehicle.model == model:
+                vehicle.avaiable = True
+                print("Vehicle  Returned successfully")
+                return
+        else:
+            print(f"Invalid model name {model}")
+             
 # Create specific vehicle objects
 car1 = Car("Hyundai", "i20", 40, doors=4)
 bike1 = Motorcycle("Bajaj", "Pulsar NS200", 25, engine_cc=200)
 bike2 = Motorcycle("Yamaha", "FZ-S", 22, engine_cc=149)
 my_store = RentalStore("Biraj_Rental")
-    # Add vehicles to the store list
-print("--- Registering Vehicles ---")
+#     # Add vehicles to the store list
+# print("--- Registering Vehicles ---")
 my_store.add_vehicle(car1)
 my_store.add_vehicle(bike1)
 my_store.add_vehicle(bike2)
-
     # Show the entire fleet using our custom loop
-my_store.show_fleet()      
+my_store.show_fleet() 
+my_store.rent_vehicle("Pulsar NS200",3)
+my_store.return_vehicle("Pulsar NS200")
      
 
